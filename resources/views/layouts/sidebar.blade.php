5c5c4ed647d14e63e3f39dbc8914b1bc3035220f
hoanganh <nguyenhoanganh01102002@gmail.com>
2024-02-21 13:33:27 +0700

    edit

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,16 +1,16 @@
 <div class="bg-dark h-100 w-50">
-    <div class="border border-white text-white p-3">
+    <div class="border border-white p-3">
         <a href="{{ route('department.index') }}" class="text-decoration-none text-white">Department</a>
     </div>
 
-    <div class="border border-white text-white p-3">
-        <a href="#" style="text-decoration: none;color: white">Student</a>
+    <div class="border border-white p-3">
+        <a href="#" class="text-decoration-none text-white">Student</a>
     </div>
-    <div class="border border-white text-white p-3">
-        <a href="{{route('course.index')}}" style="text-decoration: none;color: white">Course</a>
+    <div class="border border-white p-3">
+        <a href="{{route('course.index')}}" class="text-decoration-none text-white">Course</a>
     </div>
-    <div class="border border-white text-white p-3">
-        <a href="#" style="text-decoration: none;color: white">Result</a>
+    <div class="border border-white p-3">
+        <a href="#" class="text-decoration-none text-white">Result</a>
     </div>
 </div>
 
